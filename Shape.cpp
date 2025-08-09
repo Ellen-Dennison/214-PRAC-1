168faaab7b842ede3e91d5b80e464f7a4e68ea02
NomziPhosa <157846833+NomziPhosa@users.noreply.github.com>
2025-08-09 19:07:34 +0200

    shape.cpp

---
+++ Shape.cpp
@@ -0,0 +1,9 @@
+#include "Shape.h"
+
+Shape::Shape(int length, int width, std::string colour, int position_x, int position_y){
+    this.length = length;
+    this.width = width;
+    this.colour = colour;
+    this.position_x = position_x;
+    this.position_y = position_y;
+}
