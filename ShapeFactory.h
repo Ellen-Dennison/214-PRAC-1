fee8a8bc887f4bfb1f15f639a4752b60b79bce06
Ellen-Dennison <87267126+Ellen-Dennison@users.noreply.github.com>
2025-08-11 20:22:58 +0200

    destructors

--- ShapeFactory.h
+++ ShapeFactory.h
@@ -1,12 +1,12 @@
 #ifndef SHAPEFACTORY_H
 #define SHAPEFACTORY_H
+#include "Shape.h"
 
 class ShapeFactory
 {  
     protected:
     virtual Shape* createshape() = 0;
     virtual void toString() = 0;
-
 };
 
 #endif
